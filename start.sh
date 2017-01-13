2ecf9e73e146d19738b7dd1254f3ca71011e40ec
Vishwa <v1shwa@users.noreply.github.com>
2017-01-13 18:25:11 +0530

    Fixes #1

--- start.sh
+++ start.sh
@@ -10,28 +10,33 @@ function is_installed {
 echo "Configuring your dev environment.. Sit tight!"
 
 # Install X-code, if not
-if ! xcode-select --install 2>&1 | grep installed; then
+xcode_dir=$(/usr/bin/xcode-select -print-path 2>/dev/null)
 
+if ! [ -d "$xcode_dir" ]; then
+    echo "Command Line Tools is missing. Attempting to install.."
+    xcode-select --install
     # Wait for xcode to install
-    echo -n "Waiting for Commandline Dev tools to install."
+    echo -n "Waiting for Commandline tools to install."
     while true
     do
-        if xcode-select --install 2>&1 | grep installed; then
-            echo "Commandline dev tools is installed successfully.. Moving on.."
+        if ! [ -d "$xcode_dir" ]; then
+            echo "Command Line Tools is installed successfully"
             break
         fi
         echo -n "."
-        sleep 0.5s
+        sleep 1s
     done
 fi
 
 # Install pip
 if [[ $(is_installed pip) -eq 0 ]]; then
+    echo "Installing PIP"
     sudo easy_install pip
 fi
 
 # Install ansible
 if [[ $(is_installed ansible) -eq 0 ]]; then
+    echo "Installing Ansible"
     sudo pip install ansible
 fi
 
